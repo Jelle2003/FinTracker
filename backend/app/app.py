d2528cbff8b649f8ccde72f0e845a130c7a0c54f
jelle <jelle.kerkhofs@outlook.com>
2026-09-10 19:55:31 +0200

    List Expenses in Flask Table

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, make_response, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
-from datetime import date
+from datetime import date, datetime
 
 
 app = Flask(
@@ -10,6 +10,7 @@ app = Flask(
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///expenses.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'my-secret-key'
 db = SQLAlchemy(app)
 
 class Expense(db.Model):
@@ -18,7 +19,7 @@ class Expense(db.Model):
     amount = db.Column(db.Float, nullable=False)
     category = db.Column(db.String(50), nullable=False)
     date = db.Column(db.Date, nullable=False, default=date.today)
-    
+
 #create the database
 with app.app_context():
     db.create_all()
@@ -28,7 +29,48 @@ with app.app_context():
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+
+    expenses =  Expense.query.order_by(Expense.date.desc(), Expense.id.desc()).all()
+    print(expenses)
+    return render_template("index.html", expenses=expenses)
+
+@app.route("/add", methods=['POST'])
+def add():
+
+    description = (request.form.get("description") or "").strip()
+    amount_str = (request.form.get("amount") or "").strip()
+    category = (request.form.get("category") or "").strip()
+    date_str = (request.form.get("date") or "").strip()
+
+    if not description or not amount_str or not category:
+        flash("Please fill description, amount and category", "error")
+        return redirect(url_for("index"))
+
+    try:
+        amount = float(amount_str)
+        if amount <= 0:
+            raise ValueError
+
+    except ValueError:
+        flash("Amount must be a postive number", "error")
+        return redirect(url_for("index"))
+
+    try:
+        d = datetime.strptime(date_str, "%Y-%m-%d").date() if date_str else date.today()
+
+    except ValueError:
+        d = date.today
+
+    e = Expense(description=description, amount=amount, category=category, date=d)
+    db.session.add(e)
+    db.session.commit()
+
+    flash("Expense added", "success")
+    return redirect(url_for("index"))
+
+    print("Form received:", dict(request.form))
+    return make_response("Form received check the console")
+
 
 
 if __name__ == "__main__":
