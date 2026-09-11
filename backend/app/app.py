d1a08040704bc52c7651e99c2d65bb24d3ea51f9
jelle <jelle.kerkhofs@outlook.com>
2026-09-11 13:17:03 +0200

    Add daily Taotal Chart

--- backend/app/app.py
+++ backend/app/app.py
@@ -89,6 +89,25 @@ def index():
     cat_values = [round(float(s or 0), 2) for _, s in cat_rows]
     print(cat_values)
 
+
+    # Day Chart
+    day_q = db.session.query(Expense.date, func.sum(Expense.amount))
+    
+    if start_date:
+        day_q = day_q.filter(Expense.date >= start_date)
+
+    if end_date:
+        day_q = day_q.filter(Expense.date <= end_date)
+
+    if selected_category:
+        day_q = day_q.filter(Expense.category == selected_category)
+
+    day_rows = day_q.group_by(Expense.category).order_by(Expense.date).all()
+    print(day_rows)
+    day_labels = [d.isoformat() for d, _ in day_rows]
+    day_values = [round(float(s or 0), 2) for _, s in day_rows]
+    print(day_values)
+
     
     return render_template(
         "index.html", 
@@ -101,7 +120,9 @@ def index():
         end_str=end_str,
         selected_category=selected_category,
         cat_labels=cat_labels,
-        cat_values=cat_values
+        cat_values=cat_values,
+        day_labels=day_labels,
+        day_values=day_values
         
         )
 
